902cba5f01d94f0f47b3c613b5f3d85e0137d0e0
edobasky <edore7@gmail.com>
2022-04-02 01:59:46 +0100

    updated features

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ navBtnDOM.addEventListener('click', () => {
   links.classList.toggle('show-links')
 })
 
+
 const date = getElement('#date')
-const currentYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear();
 date.textContent = currentYear
